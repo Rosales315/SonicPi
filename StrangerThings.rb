b575f5712b7d3f97144091d1a3403a64bb754f9b
Rosales315 <125306012+Rosales315@users.noreply.github.com>
2023-02-13 11:17:05 -0800

    Create StrangerThings.rb

---
+++ StrangerThings.rb
@@ -0,0 +1,24 @@
+
+# Welcome to Sonic Pi
+
+use_synth :saw
+use_bpm 85
+live_loop :strange do
+  play:c4
+  sleep 0.5
+  play:e4
+  sleep 0.5
+  play:g4
+  sleep 0.5
+  play:b4
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:b4
+  sleep 0.5
+  play:g4
+  sleep 0.5
+  play:e4
+  sleep 0.5
+  
+end
